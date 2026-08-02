bfb7c506cff293a976f27b8371457738f1fe98e6
MOHIT BHATIA <mohitbhatia612@gmail.com>
2026-08-02 11:53:03 -0400

    chore: made some configuration changes for deployment over vercel and render

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,12 +2,20 @@ const express = require("express");
 const cookieParser = require("cookie-parser")
 const apiV1Router = require("./routes");
 const errorHandler = require("./middleware/errorHandler.middleware")
+const cors = require("cors")
 
 
 const app = express();
 
 app.use(express.json())
 app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+)
 
 app.get("/api/v1/health", (req, res) => {
   res.status(200).json({ status: "ok" });
--- frontend/src/shared/services/apiClient.js
+++ frontend/src/shared/services/apiClient.js
@@ -1,8 +1,10 @@
 import axios from "axios"
 
 const apiClient = axios.create({
-    baseURL: "/api/v1",
-    withCredentials: true
+  baseURL: import.meta.env.VITE_API_URL
+    ? `${import.meta.env.VITE_API_URL}/api/v1`
+    : "/api/v1",
+  withCredentials: true,
 })
 
 export default apiClient;
